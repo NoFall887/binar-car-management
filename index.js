c219314dcb73f4568b2f8d9317355a61fefd697e
NoFall887 <naufalamiruddin123456@gmail.com>
2022-09-30 10:24:06 +0700

    list car page implementation

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "views")));
 
+app.get("/");
+
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
